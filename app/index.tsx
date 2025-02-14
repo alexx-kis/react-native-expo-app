84fbe282cbdc49c97cc4f08deb0fe458839bf326
Alexander <fenaethylaamiinloever@gmail.com>
2025-02-14 22:32:53 +0300

    fixed

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import CustomButton from '@/components/custom-button';
-import { Image, SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native';
-import images from '../constants/images';
 import { router } from 'expo-router';
+import { Image, Platform, SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native';
+import images from '../constants/images';
 
 // #======================== index ========================# //
 
@@ -21,25 +21,40 @@ export default function HomeScreen() {
             resizeMode='contain'
           />
           <View className='mt-5'>
-            <Text className=' relative text-3xl text-white font-bold text-center'>
+            <Text className='text-3xl text-white font-bold text-center'>
               Discover Endless Possibilities with &nbsp;
-              <Text className='text-secondary-200'>
-                Aora
+              <View className='relative inline-flex'>
+                <View style={{
+                  transform: Platform.OS === 'ios' ? [{ translateY: 14 }] : [],
+                  position: 'relative',
+                }}>
+                  <Text
+                    className='text-secondary-200 text-3xl font-bold'
+                  >
+                    Aora
+                  </Text>
+                </View>
                 <Image
                   source={images.path}
-                  style={{ width: 146, height: 15 }}
                   resizeMode='contain'
-                  className='absolute w-[136px] h-[15px] -bottom-2 -right-8'
+                  className='absolute w-[136px] h-[15px] -right-8'
+                  style={{
+                    width: 146, 
+                    height: 15, 
+                    position: 'absolute',
+                    bottom: Platform.OS === 'ios' ? -22 : -8
+                  }}
                 />
-              </Text>
+              </View>
             </Text>
           </View>
+
           <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
             Where creativity meets innovation: embark on a journey of limitless exploration with Aora
           </Text>
           <CustomButton
             title='Continue with Email'
-            handlePress={() => { router.push('/sign-in')}}
+            handlePress={() => { router.push('/sign-in'); }}
             containerStyles='w-full mt-7'
             textStyles=''
             isLoading={false}
@@ -47,7 +62,7 @@ export default function HomeScreen() {
         </View>
       </ScrollView>
 
-      <StatusBar backgroundColor='#161622'/>
+      <StatusBar backgroundColor='#161622' />
     </SafeAreaView>
   );
 }
